0bb0f1b6e6fbadf3007081b1339b0a8bd0e0e1f1
Mark Impossible <i13denmark@gmail.com>
2021-03-01 19:45:11 +0700

    Updating the code

---
+++ updated-code/TransitionPoint.cpp
@@ -0,0 +1,20 @@
+#include "TransitionPoint.h"
+#include "global.h"
+
+force TransitionPoint::get_force() {
+	if (index == spring_parts) {
+		return (-parts[index - 1]->get_force());
+	}
+	else if (index == 0) return (parts[index]->get_force());
+	else {
+		return (parts[index]->get_force() - parts[index - 1]->get_force());
+	}
+}
+
+acceleration TransitionPoint::get_acceleration() {
+	return ((get_force()) / weight);
+}
+
+void TransitionPoint::update_acceleration() {
+	point_acceleration = get_acceleration();
+}
